92fb59e5813ffc2402831d8434283f83fe6eb007
Jieoi <66547561+Jieoi@users.noreply.github.com>
2023-06-08 15:22:07 +0800

    removed testing files

--- starter.py
+++
@@ -1,8 +0,0 @@
-import pybullet as p
-import pybullet_data as pd
-
-p.connect(p.GUI)
-
-plane_shape = p.createCollisionShape(p.GEOM_PLANE)
-floor = p.createMultiBody(plane_shape, plane_shape)
-p.setGravity(0,0,-10)
