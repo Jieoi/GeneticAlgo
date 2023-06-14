3b264ba90b33a6334e6a44decc56ac3b8ea43b6c
Jieoi <66547561+Jieoi@users.noreply.github.com>
2023-06-14 15:06:12 +0800

    succussful implementation of the simulation.

---
+++ genatic/motor_test.py
@@ -0,0 +1,40 @@
+import pybullet as p
+import pybullet_data as pd
+import creature 
+import time
+
+# starter code
+p.connect(p.GUI)
+
+##do not cache the urdf so that editing will be live
+p.setPhysicsEngineParameter(enableFileCaching=0)
+##get rid of the panels
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+plane_shape = p.createCollisionShape(p.GEOM_PLANE)
+floor = p.createMultiBody(plane_shape, plane_shape)
+p.setGravity(0,0,-10)
+
+# generate random creature with gene
+c = creature.Creature(gene_count = 5)
+
+# save to XML
+with open('test.urdf','w') as f:
+    c.get_expanded_links()
+    f.write(c.to_xml())
+
+# load the urdf file
+cid = p.loadURDF('test.urdf')
+
+# set real time simulation to true
+p.setRealTimeSimulation(1)
+
+# infinite loop to iterate
+while True:
+    for jid in range(p.getNumJoints(cid)):
+        m =c.get_motors()[jid]
+        # id of creature:cid
+        p.setJointMotorControl2(cid,jid,
+            controlMode = p.VELOCITY_CONTROL,
+            targetVelocity = m.get_output(),
+            force = 5)
+    time.sleep(0.1) # update the motor every 0.1 sec
